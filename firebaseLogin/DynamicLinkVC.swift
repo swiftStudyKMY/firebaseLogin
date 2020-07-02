b2f7ae0585aee666e5585633191cf03bce4cee04
KMY <muziq43@hanafn.com>
2020-07-02 15:31:39 +0900

    dynamic Link domainURL을 찾지 못해 미완성

--- firebaseLogin/DynamicLinkVC.swift
+++ firebaseLogin/DynamicLinkVC.swift
@@ -16,7 +16,26 @@ class DynamicLinkVC:UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //domainURL 찾지 못해서 아직 미완성
+        let compoents = DynamicLinkComponents(link: URL(string: "https://naver.com")!, domainURIPrefix: "")
         
+        let iOSParams = DynamicLinkIOSParameters(bundleID: "com.kmy.firebaseLogin")
+        
+        //https://apps.apple.com/kr/app/apple-store/id375380948
+        iOSParams.appStoreID = "375380948"
+        
+        compoents?.iOSParameters = iOSParams
+        print(">>>\(compoents?.url?.absoluteString)")
+        
+        let option = DynamicLinkComponentsOptions()
+        option.pathLength = .short
+        
+        compoents?.options = option
+        
+        compoents?.shorten(completion: { (url, warning, error) in
+            print(">>>>>>\(url)")
+            
+        })
     }
     
     
